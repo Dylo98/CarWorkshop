02b8d04fe175c1f602542a7319fc29386d5edda4
Dawid Dyl <dawid.dyl@microsoft.wsei.edu.pl>
2024-02-08 17:21:37 +0100

    Add properties to class. Complete model CarWorkshop in Domain

--- CarWorkshop.Domain/Entities/CarWorkshop.cs
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -8,9 +8,14 @@ namespace CarWorkshop.Domain.Entities
 {
     public class CarWorkshop
     {
+        public required int Id { get; set; }
         public string Name { get; set; } = default!;
         public string? Description { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
+
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ","-");
     }
 }
